7476e4e3438c41ee84a91b60d8b296e85af8c001
Ash-exwo <aswathygr61@gmail.com>
2026-07-15 01:02:02 +0530

    View Volunteer v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import AddVolunteer from "./components/AddVolunteer"
+import ViewVolunteer from "./components/ViewVolunteer"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/add-volunteer" element={<AddVolunteer/>}/>
+      <Route path="/view-volunteer" element={<ViewVolunteer/>}/>
     </Routes>
     </BrowserRouter>
     </>
